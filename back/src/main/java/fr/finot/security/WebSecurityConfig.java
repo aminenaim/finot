24ba90c375c080d4f0c584c25dd012acc0fc85c4
RemiSaurel <r.saurel5@gmail.com>
2025-01-15 00:03:34 +0100

    fix: cors

---
+++ back/src/main/java/fr/finot/security/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package fr.finot.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Security configuration.
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+
+    private final Environment environment;
+
+    public WebSecurityConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll() // Allow all requests without authentication
+                )
+                .securityContext(AbstractHttpConfigurer::disable) // Disable security context
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // Customize headers if needed
+                .csrf(AbstractHttpConfigurer::disable) // Explicitly disable CSRF
+                .cors(AbstractHttpConfigurer::disable); // Explicitly disable CORS
+
+        return http.build();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*")
+                        .allowCredentials(true); // Allow credentials (cookies) in the CORS response
+            }
+        };
+    }
+
+}
--- front/app/pages/index.vue
+++ front/app/pages/index.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const input = ref<string>("");
+const response = ref<string>("");
 const runtimeConfig = useRuntimeConfig();
 const send = async () => {
-  const res = await $fetch("/ai/hello", {
+  response.value = await $fetch("/ai/hello", {
     baseURL: runtimeConfig.public.apiUrl,
     method: "POST",
     body: JSON.stringify({ data: input.value }),
   });
-  console.log(res);
 };
 </script>
 
@@ -16,7 +16,7 @@ const send = async () => {
     <UContainer
       class="w-full h-full flex flex-col justify-center items-center pt-24"
     >
-      <p class="text-center mt-4">{{ input }}</p>
+      <p class="text-center mt-4">{{ response }}</p>
 
       <div class="absolute w-full max-w-3xl bottom-24 flex gap-4">
         <UInput v-model="input" placeholder="Type something" class="w-full" />
