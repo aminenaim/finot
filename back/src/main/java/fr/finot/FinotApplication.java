0586e778867180cb531a4b223edc4fbb5f90bee9
RemiSaurel <r.saurel5@gmail.com>
2025-01-14 19:55:59 +0100

    chore: init back

---
+++ back/src/main/java/fr/finot/FinotApplication.java
@@ -0,0 +1,13 @@
+package fr.finot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinotApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/fr/finot/FinotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.finot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
