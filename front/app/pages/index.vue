7a71dac8a4e1168c8dbdf25a5f0c0b026457129e
RemiSaurel <r.saurel5@gmail.com>
2025-01-14 19:49:59 +0100

    chore: init front

---
+++ front/app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <UButton class="mt-14 ml-14" @click="console.log('Hello Finot')"
+    >Click me!</UButton
+  >
+</template>
---
+++ front/nuxt.config.ts
@@ -0,0 +1,20 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/icon"],
+  runtimeConfig: {
+    apiUrl: "",
+  },
+  ssr: false,
+});
