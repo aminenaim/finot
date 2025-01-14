04774c621aa49fbf1ce7fa2ff223027a7d2e1f19
RemiSaurel <r.saurel5@gmail.com>
2025-01-14 20:37:48 +0100

    chore: setup back

---
+++ back/src/main/java/fr/finot/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package fr.finot.rabbitmq;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.elaastic.queue}")
+    private String queueName;
+
+    @Value("${rabbitmq.elaastic.exchange}")
+    private String exchangeName;
+
+    @Bean
+    public Queue queue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(queueName);
+    }
+}
+
---
+++ back/src/main/resources/db/migration/V1__Init_DB.sql
@@ -0,0 +1,6 @@
+CREATE TABLE User (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) NOT NULL,
+);
+
+INSERT INTO User (username) VALUES ('gadget');
